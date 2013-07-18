6c57d53fa92e727ad02d26a9d3752a07c0ff3c24
farmerli <lixu_01@126.com>
2013-07-18 22:29:51 +0800

    tree

--- struct/tree.php
+++ struct/tree.php
@@ -1,14 +1,30 @@
 <?php
 class node
 {
+	public $index;
+
 	public $data;
 
 	public $lchild;
 
 	public $rchild;
 
+	public function __construct($data, $index) {
+		$this->data = $data;
+	}
+
 }
 
 $a = [3,41,32,44,51,61,1,53,23,546,12];
 
-$tree = 
+$tree = null;
+
+class BiTree
+{
+	$this->root = null;
+
+	public function __construct()
+	{
+		
+	}
+}
